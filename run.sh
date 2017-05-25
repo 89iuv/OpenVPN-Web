5aa847e415945f8de50d4195c192978b61c3b329
Valentin Uveges <valentin.uveges@gmail.com>
2017-05-25 13:44:15 +0200

    update deploy script

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-mvn clean install
+#install bower components
+DIR=`pwd`
+cd ./src/main/resources/public/
+bower install --allow-root
+cd $DIR
+
+#run maven
+mvn clean install -DskipTests
+
+#configure app properties
 cp ./src/main/resources/application.properties ./target/
 sed 's|src/test/resources/openvpn-status.log|/etc/openvpn/openvpn-status.log|' ./target/application.properties
+
+#run application
+kill `ps -C "java -jar ./target/otho-1.0-SNAPSHOT.jar" -o pid=`
 nohup java -jar ./target/otho-1.0-SNAPSHOT.jar &
