0853862f793cf866cb8642ac53fff0e29f68fdbe
Valentin Uveges <valentin.uveges@gmail.com>
2017-05-25 14:49:29 +0200

    change the execution location of the app

--- run.sh
+++ run.sh
@@ -14,5 +14,6 @@ cp ./src/main/resources/application.properties ./target/
 sed -i 's|src/test/resources/openvpn-status.log|/etc/openvpn/openvpn-status.log|' ./target/application.properties
 
 #run application
-kill `ps -C "java -jar ./target/otho-1.0-SNAPSHOT.jar" -o pid=`
-nohup java -jar ./target/otho-1.0-SNAPSHOT.jar &
+cd ./target/
+kill `ps -C "java -jar ./otho-1.0-SNAPSHOT.jar" -o pid=`
+nohup java -jar ./otho-1.0-SNAPSHOT.jar &
